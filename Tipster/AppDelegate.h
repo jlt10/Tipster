ed0c8cf6a6d3b9eb72e719e44dec82248677cf57
Jamie Tan <jamietan@fb.com>
2018-06-26 13:28:48 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Jamie Tan on 6/26/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
